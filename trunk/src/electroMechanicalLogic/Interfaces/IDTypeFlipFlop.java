93a3a4b13ee80300f1ae938483b1afbc84787618
perrinelson1 <perrinelson1@d2c7111a-e0d9-4fb1-924d-996a1433c88b>
2014-02-14 03:12:06 +0000

    Level Triggered D Type Flip Flop

---
+++ trunk/src/electroMechanicalLogic/Interfaces/IDTypeFlipFlop.java
@@ -0,0 +1,24 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic.Interfaces;
+
+public interface IDTypeFlipFlop {
+
+	public abstract boolean getQ();
+
+	public abstract boolean getQBar();
+
+	public abstract void setClk(boolean value);
+
+	public abstract void setD(boolean value);
+
+	public abstract void setPower(boolean value);
+
+	public abstract void step();
+}
