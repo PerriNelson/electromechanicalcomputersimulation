dd0af3fcc3654ad98f66bb67c940a0639daa903d
perrinelson1 <perrinelson1@d2c7111a-e0d9-4fb1-924d-996a1433c88b>
2014-02-14 01:25:00 +0000

    RS Flip-Flop

---
+++ trunk/src/electroMechanicalLogic/Interfaces/RSFlipFlopTest.java
@@ -0,0 +1,133 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic.Interfaces;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import electroMechanicalLogic.RSFlipFlop;
+
+public class RSFlipFlopTest {
+
+	private IRSFlipFlop systemUnderTest;
+
+	@Before
+	public void setUp() throws Exception {
+		systemUnderTest = new RSFlipFlop();
+		systemUnderTest.setPower(true);
+	}
+
+	@Test
+	public void test_GetQ_WhenReset_ReturnsOff() {
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQ_WhenResetAndResetTurnedOff_RemainsOff() {
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+		systemUnderTest.setR(false);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQ_WhenSet_ReturnsOn() {
+		systemUnderTest.setS(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQ_WhenSetAndReSetTogether_ReturnsOff() {
+		systemUnderTest.setS(true);
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQ_WhenSetAndSetTurnedOff_RemainsOn() {
+		systemUnderTest.setS(true);
+		systemUnderTest.step();
+		systemUnderTest.setS(false);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQ_WhenUninitialized_ReturnsOn() {
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+	}
+
+	@Test
+	public void test_GetQBar_WhenReset_ReturnsOn() {
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public void test_GetQBar_WhenResetAndResetTurnedOff_RemainsOn() {
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+		systemUnderTest.setR(false);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public void test_GetQBar_WhenSet_ReturnsOff() {
+		systemUnderTest.setS(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public void test_GetQBar_WhenSetAndReSetTogether_ReturnsOff() {
+		systemUnderTest.setS(true);
+		systemUnderTest.setR(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public void test_GetQBar_WhenSetAndSetTurnedOff_RemainsOff() {
+		systemUnderTest.setS(true);
+		systemUnderTest.step();
+		systemUnderTest.setS(false);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public void test_GetQBar_WhenUninitialized_ReturnsOn() {
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQBar());
+	}
+
+}
---
+++ trunk/src/electroMechanicalLogic/RSFlipFlop.java
@@ -0,0 +1,52 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic;
+
+import electroMechanicalLogic.Interfaces.IRSFlipFlop;
+import electroMechanicalLogic.Interfaces.ITwoInputSingleOutputGate;
+
+public class RSFlipFlop implements IRSFlipFlop {
+
+	public ITwoInputSingleOutputGate norR = new TwoInputNOrGate();
+	public ITwoInputSingleOutputGate norS = new TwoInputNOrGate();
+
+	@Override
+	public boolean getQ() {
+		return norR.getOutput();
+	}
+
+	@Override
+	public boolean getQBar() {
+		return norS.getOutput();
+	}
+
+	@Override
+	public void setPower(boolean value) {
+		norR.setPower(value);
+		norS.setPower(value);
+	}
+
+	@Override
+	public void setR(boolean value) {
+		norR.setB(value);
+	}
+
+	@Override
+	public void setS(boolean value) {
+		norS.setA(value);
+	}
+
+	@Override
+	public void step() {
+		norR.setA(norS.getOutput());
+		norS.setB(norR.getOutput());
+		norR.step();
+		norS.step();
+	}
+}
