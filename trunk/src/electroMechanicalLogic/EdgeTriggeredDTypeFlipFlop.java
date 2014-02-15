52bb94a50b4e8d0a3a2204c5abfc0cd8836ae5dd
perrinelson1 <perrinelson1@d2c7111a-e0d9-4fb1-924d-996a1433c88b>
2014-02-15 02:13:03 +0000

    Edge Triggered D Type Flip Flop

---
+++ trunk/src/electroMechanicalLogic/EdgeTriggeredDTypeFlipFlop.java
@@ -0,0 +1,82 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic;
+
+import electroMechanicalLogic.Interfaces.IDTypeFlipFlop;
+import electroMechanicalLogic.Interfaces.IRSFlipFlop;
+import electroMechanicalLogic.Interfaces.IRelay;
+import electroMechanicalLogic.Interfaces.ITwoInputSingleOutputGate;
+
+public class EdgeTriggeredDTypeFlipFlop implements IDTypeFlipFlop {
+	private IRSFlipFlop stage1 = new RSFlipFlop();
+	private IRSFlipFlop stage2 = new RSFlipFlop();
+	private ITwoInputSingleOutputGate sAnd1 = new TwoInputAndGate();
+	private ITwoInputSingleOutputGate rAnd1 = new TwoInputAndGate();
+	private ITwoInputSingleOutputGate sAnd2 = new TwoInputAndGate();
+	private ITwoInputSingleOutputGate rAnd2 = new TwoInputAndGate();
+	private IRelay dBar = new Inverter();
+	private IRelay clkBar = new Inverter();
+
+	@Override
+	public boolean getQ() {
+		return stage2.getQ();
+	}
+
+	@Override
+	public boolean getQBar() {
+		return stage2.getQBar();
+	}
+
+	@Override
+	public void setClk(boolean value) {
+		clkBar.setInput(value);
+		sAnd2.setB(value);
+		rAnd2.setA(value);
+	}
+
+	@Override
+	public void setD(boolean value) {
+		dBar.setInput(value);
+		rAnd1.setB(value);
+	}
+
+	@Override
+	public void setPower(boolean value) {
+		dBar.setPower(value);
+		clkBar.setPower(value);
+		sAnd1.setPower(value);
+		rAnd1.setPower(value);
+		stage1.setPower(value);
+		sAnd2.setPower(value);
+		rAnd2.setPower(value);
+		stage2.setPower(value);
+	}
+
+	@Override
+	public void step() {
+		clkBar.step();
+		dBar.step();
+		sAnd1.setA(dBar.getOutput());
+		sAnd1.setB(clkBar.getOutput());
+		rAnd1.setA(clkBar.getOutput());
+		sAnd1.step();
+		rAnd1.step();
+		stage1.setS(sAnd1.getOutput());
+		stage1.setR(rAnd1.getOutput());
+		stage1.step();
+		sAnd2.setA(stage1.getQBar());
+		rAnd2.setB(stage1.getQ());
+		sAnd2.step();
+		rAnd2.step();
+		stage2.setS(sAnd2.getOutput());
+		stage2.setR(rAnd2.getOutput());
+		stage2.step();
+	}
+
+}
---
+++ trunk/src/electroMechanicalLogic/Tests/EdgeTriggeredDTypeFlipFlopTest.java
@@ -0,0 +1,128 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import electroMechanicalLogic.Interfaces.IDTypeFlipFlop;
+import electroMechanicalLogic.EdgeTriggeredDTypeFlipFlop;
+
+public class EdgeTriggeredDTypeFlipFlopTest {
+
+	IDTypeFlipFlop systemUnderTest;
+	
+	@Before
+	public void setUp() throws Exception {
+		systemUnderTest = new EdgeTriggeredDTypeFlipFlop();
+		systemUnderTest.setPower(true);
+		systemUnderTest.step();
+	}
+
+	@Test
+	public final void test_GetQ_WhenClockIsOnAndDataIsOff_ReturnsOff() {
+		systemUnderTest.setD(false);
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQ());
+	}
+
+	@Test
+	public final void test_GetQ_WhenDataIsOnAndClockTransitionsToOn_ReturnsOn() {
+		systemUnderTest.setD(true);
+		systemUnderTest.setClk(false);
+		systemUnderTest.step();
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+	}
+
+	@Test
+	public final void test_GetQ_WhenClockIsOnAndDataIsToggled_DoesNotChange() {
+		systemUnderTest.setD(true);
+		systemUnderTest.step();
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+
+		systemUnderTest.setD(false);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQ());
+	}
+
+	@Test
+	public final void test_GetQ_WhenClockAndDataBothTransistionToTrue_ReturnsFalse() {
+		systemUnderTest.setD(false);
+		systemUnderTest.setClk(false);
+		systemUnderTest.step();
+		
+		systemUnderTest.setD(true);
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQ());
+	}
+
+
+
+	@Test
+	public final void test_GetQBar_WhenClockIsOnAndDataIsOff_ReturnsOn() {
+		systemUnderTest.setD(false);
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public final void test_GetQBar_WhenDataIsOnAndClockTransitionsToOn_ReturnsOff() {
+		systemUnderTest.setD(true);
+		systemUnderTest.setClk(false);
+		systemUnderTest.step();
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public final void test_GetQBar_WhenClockIsOnAndDataIsToggled_DoesNotChange() {
+		systemUnderTest.setD(true);
+		systemUnderTest.step();
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+
+		systemUnderTest.setD(false);
+		systemUnderTest.step();
+
+		assertFalse(systemUnderTest.getQBar());
+	}
+
+	@Test
+	public final void test_GetQBar_WhenClockAndDataBothTransistionToTrue_ReturnsTrue() {
+		systemUnderTest.setD(false);
+		systemUnderTest.setClk(false);
+		systemUnderTest.step();
+		
+		systemUnderTest.setD(true);
+		systemUnderTest.setClk(true);
+		systemUnderTest.step();
+
+		assertTrue(systemUnderTest.getQBar());
+	}
+
+}
