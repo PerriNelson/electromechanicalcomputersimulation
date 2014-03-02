f8801f259a9117e2a2355c1c8dd969e4032c8be8
perrinelson1 <perrinelson1@d2c7111a-e0d9-4fb1-924d-996a1433c88b>
2014-03-02 04:28:43 +0000

    SixteenByOneBitRandomAccessMemory and tests.

---
+++ trunk/src/electroMechanicalLogic/SixteenByOneBitRandomAccessMemory.java
@@ -0,0 +1,87 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic;
+
+import electroMechanicalLogic.Interfaces.IEightByOneBitRandomAccessMemory;
+import electroMechanicalLogic.Interfaces.IOneLineToTwoLineDecoder;
+import electroMechanicalLogic.Interfaces.ISixteenByOneBitRandomAccessMemory;
+import electroMechanicalLogic.Interfaces.ITwoLineToOneLineSelector;
+
+public class SixteenByOneBitRandomAccessMemory implements
+		ISixteenByOneBitRandomAccessMemory {
+	
+	private IOneLineToTwoLineDecoder decoder = new OneLineToTwoLineDecoder();
+	private ITwoLineToOneLineSelector selector = new TwoLineToOneLineSelector();
+	private IEightByOneBitRandomAccessMemory bank0 = new EightByOneBitRandomAccessMemory();
+	private IEightByOneBitRandomAccessMemory bank1 = new EightByOneBitRandomAccessMemory();
+
+	@Override
+	public boolean getDO() {
+		return selector.getQ();
+	}
+
+	@Override
+	public void setA0(boolean value) {
+		bank0.setA0(value);
+		bank1.setA0(value);
+	}
+
+	@Override
+	public void setA1(boolean value) {
+		bank0.setA1(value);
+		bank1.setA1(value);
+	}
+
+	@Override
+	public void setA2(boolean value) {
+		bank0.setA2(value);
+		bank1.setA2(value);
+	}
+
+	@Override
+	public void setDI(boolean value) {
+		bank0.setDI(value);
+		bank1.setDI(value);
+	}
+
+	@Override
+	public void setW(boolean value) {
+		decoder.setInput(value);
+	}
+
+	@Override
+	public void setPower(boolean value) {
+		decoder.setPower(value);
+		bank0.setPower(value);
+		bank1.setPower(value);
+		selector.setPower(value);
+	}
+
+	@Override
+	public void step() {
+		decoder.step();
+		
+		bank0.setW(decoder.getO0());
+		bank0.step();
+		
+		bank1.setW(decoder.getO1());
+		bank1.step();
+		
+		selector.setA(bank0.getDO());
+		selector.setB(bank1.getDO());
+		selector.step();
+	}
+
+	@Override
+	public void setA3(boolean value) {
+		decoder.setSelect(value);
+		selector.setSelect(value);
+	}
+
+}
---
+++ trunk/src/electroMechanicalLogic/Tests/SixteenByOneBitRandomAccessMemoryTest.java
@@ -0,0 +1,60 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import electroMechanicalLogic.SixteenByOneBitRandomAccessMemory;
+import electroMechanicalLogic.Interfaces.ISixteenByOneBitRandomAccessMemory;
+
+public class SixteenByOneBitRandomAccessMemoryTest {
+	private static ISixteenByOneBitRandomAccessMemory systemUnderTest;
+	private static final int bit0 = 0x01;
+	private static final int bit1 = 0x02;
+	private static final int bit2 = 0x04;
+	private static final int bit3 = 0x08;
+
+	@Before
+	public void setUp() throws Exception {
+		systemUnderTest = new SixteenByOneBitRandomAccessMemory();
+		systemUnderTest.setPower(true);
+	}
+
+	@Test
+	public final void test_GetDO_ForAllAddresses_ReturnsStoredValue() {
+		for (int i = 0; i < 16; i++){
+			for (int a = 0; a < 16; a++) {
+				setA(a);
+				systemUnderTest.setDI(a == i);
+				systemUnderTest.setW(true);
+				systemUnderTest.step();
+				systemUnderTest.setW(false);
+				systemUnderTest.step();
+			}
+			
+			for (int a = 0; a < 16; a++) {
+				setA(a);
+				systemUnderTest.step();
+				
+				assertEquals(systemUnderTest.getDO(), a == i);
+			}
+		}
+	}
+	
+	private void setA(int value) {
+		systemUnderTest.setA0((value & bit0) == bit0);
+		systemUnderTest.setA1((value & bit1) == bit1);
+		systemUnderTest.setA2((value & bit2) == bit2);
+		systemUnderTest.setA3((value & bit3) == bit3);
+	}
+	
+}
