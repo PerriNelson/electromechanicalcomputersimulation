cc8366bdf8e50c296102ba1798d6a05d8705f9f1
Perri Nelson <perri@perrinelson.com>
2014-03-08 21:47:10 +0000

    Sixty Four Kilobyte RAM

---
+++ src/electroMechanicalLogic/SixtyFourKilobyteRAM.java
@@ -0,0 +1,71 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic;
+
+import electroMechanicalLogic.Interfaces.ISixtyFourKilobyteRAM;
+import electroMechanicalLogic.Interfaces.IThirtyTwoKilobyteRAM;
+
+public class SixtyFourKilobyteRAM extends OneKilobyteRAM implements
+		ISixtyFourKilobyteRAM {
+
+	public SixtyFourKilobyteRAM() {
+		super();
+		bank0 = new ThirtyTwoKilobyteRAM();
+		bank1 = new ThirtyTwoKilobyteRAM();
+	}
+
+	@Override
+	public void setA9(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setA9(value);
+		((IThirtyTwoKilobyteRAM) bank1).setA9(value);
+	}
+
+	@Override
+	public void setAA(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setAA(value);
+		((IThirtyTwoKilobyteRAM) bank1).setAA(value);
+	}
+
+	@Override
+	public void setAB(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setAB(value);
+		((IThirtyTwoKilobyteRAM) bank1).setAB(value);
+	}
+
+	@Override
+	public void setAC(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setAC(value);
+		((IThirtyTwoKilobyteRAM) bank1).setAC(value);
+	}
+
+	@Override
+	public void setAD(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setAD(value);
+		((IThirtyTwoKilobyteRAM) bank1).setAD(value);
+	}
+
+	@Override
+	public void setAE(boolean value) {
+		((IThirtyTwoKilobyteRAM) bank0).setAE(value);
+		((IThirtyTwoKilobyteRAM) bank1).setAE(value);
+	}
+
+	@Override
+	public void setAF(boolean value) {
+		w.setSelect(value);
+		dO0.setSelect(value);
+		dO1.setSelect(value);
+		dO2.setSelect(value);
+		dO3.setSelect(value);
+		dO4.setSelect(value);
+		dO5.setSelect(value);
+		dO6.setSelect(value);
+		dO7.setSelect(value);
+	}
+}
---
+++ src/electroMechanicalLogic/Tests/SixtyFourKilobyteRAMTest.java
@@ -0,0 +1,60 @@
+/*
+  ElectroMechancialComputerSimulation by Perri D. Nelson is licensed under the Creative Commons
+  Attribution-ShareAlike 3.0 Unported License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+  USA.
+ */
+
+package electroMechanicalLogic.Tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import electroMechanicalLogic.SixtyFourKilobyteRAM;
+import electroMechanicalLogic.Interfaces.ISixtyFourKilobyteRAM;
+
+public class SixtyFourKilobyteRAMTest extends ThirtyTwoKilobyteRAMTest {
+	protected static final int bitF = 0x8000;
+
+	@Override
+	protected void setA(int value) {
+		super.setA(value);
+		((ISixtyFourKilobyteRAM) systemUnderTest).setAF((value & bitF) == bitF);
+	}
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		systemUnderTest = new SixtyFourKilobyteRAM();
+		systemUnderTest.setPower(true);
+	}
+
+	@Test
+	@Override
+	public void test() {
+		for (int i = 0; i < 2; i++) {
+			for (int a = 0; a < 2; a++) {
+				setA(translateAddress(a));
+				setDI((a == i) ? 0xff : 0);
+				systemUnderTest.setW(true);
+				systemUnderTest.step();
+				systemUnderTest.setW(false);
+				systemUnderTest.step();
+			}
+
+			for (int a = 0; a < 2; a++) {
+				setA(translateAddress(a));
+				systemUnderTest.step();
+				assertEquals(getDO(), (a == i) ? 0xff : 0);
+			}
+		}
+	}
+
+	@Override
+	protected int translateAddress(int address) {
+		return address == 0 ? 0 : 0xffff;
+	}
+}
