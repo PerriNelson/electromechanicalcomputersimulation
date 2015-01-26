ccfb94f3274cd80f33a2dd6f5d65dd291cad7f3a
perrinelson1 <perrinelson1@d2c7111a-e0d9-4fb1-924d-996a1433c88b>
2015-01-26 00:56:31 +0000

    Additional sixteen bit data path utilities and refactoring of Mark VI adding machine model.

--- trunk/src/electroMechanicalLogic/SixteenBitDataPath.java
+++ trunk/src/electroMechanicalLogic/SixteenBitDataPath.java
@@ -9,12 +9,34 @@
 package electroMechanicalLogic;
 
 import electroMechanicalLogic.Interfaces.DataChannels.IEightBitAInput;
+import electroMechanicalLogic.Interfaces.DataChannels.IEightBitAOut;
 import electroMechanicalLogic.Interfaces.DataChannels.IEightBitBInput;
+import electroMechanicalLogic.Interfaces.DataChannels.IEightBitBOut;
 import electroMechanicalLogic.Interfaces.DataChannels.IEightBitDataOut;
 import electroMechanicalLogic.Interfaces.DataChannels.ISixteenBitAInput;
 import electroMechanicalLogic.Interfaces.DataChannels.ISixteenBitAOutput;
 
 public class SixteenBitDataPath {
+	public static void AOutToAIn(final IEightBitAOut sourceLow,
+			final IEightBitAOut sourceHigh, final ISixteenBitAInput target) {
+		target.setA0(sourceLow.getA0());
+		target.setA1(sourceLow.getA1());
+		target.setA2(sourceLow.getA2());
+		target.setA3(sourceLow.getA3());
+		target.setA4(sourceLow.getA4());
+		target.setA5(sourceLow.getA5());
+		target.setA6(sourceLow.getA6());
+		target.setA7(sourceLow.getA7());
+		target.setA8(sourceHigh.getA0());
+		target.setA9(sourceHigh.getA1());
+		target.setAA(sourceHigh.getA2());
+		target.setAB(sourceHigh.getA3());
+		target.setAC(sourceHigh.getA4());
+		target.setAD(sourceHigh.getA5());
+		target.setAE(sourceHigh.getA6());
+		target.setAF(sourceHigh.getA7());
+	}
+
 	public static void AOutToAIn(final ISixteenBitAOutput source,
 			final IEightBitAInput targetLow, final IEightBitAInput targetHigh) {
 		targetLow.setA0(source.getA0());
@@ -36,6 +58,26 @@ public class SixteenBitDataPath {
 		targetHigh.setA7(source.getAF());
 	}
 
+	public static void AOutToAIn(final ISixteenBitAOutput source,
+			final ISixteenBitAInput target) {
+		target.setA0(source.getA0());
+		target.setA1(source.getA1());
+		target.setA2(source.getA2());
+		target.setA3(source.getA3());
+		target.setA4(source.getA4());
+		target.setA5(source.getA5());
+		target.setA6(source.getA6());
+		target.setA7(source.getA7());
+		target.setA8(source.getA8());
+		target.setA9(source.getA9());
+		target.setAA(source.getAA());
+		target.setAB(source.getAB());
+		target.setAC(source.getAC());
+		target.setAD(source.getAD());
+		target.setAE(source.getAE());
+		target.setAF(source.getAF());
+	}
+
 	public static void AOutToBIn(final ISixteenBitAOutput source,
 			final IEightBitBInput targetLow, final IEightBitBInput targetHigh) {
 		targetLow.setB0(source.getA0());
@@ -55,6 +97,26 @@ public class SixteenBitDataPath {
 		targetHigh.setB5(source.getAD());
 		targetHigh.setB6(source.getAE());
 		targetHigh.setB7(source.getAF());
+	}
+
+	public static void BOutToAIn(final IEightBitBOut sourceLow,
+			final IEightBitBOut sourceHigh, final ISixteenBitAInput target) {
+		target.setA0(sourceLow.getB0());
+		target.setA1(sourceLow.getB1());
+		target.setA2(sourceLow.getB2());
+		target.setA3(sourceLow.getB3());
+		target.setA4(sourceLow.getB4());
+		target.setA5(sourceLow.getB5());
+		target.setA6(sourceLow.getB6());
+		target.setA7(sourceLow.getB7());
+		target.setA8(sourceHigh.getB0());
+		target.setA9(sourceHigh.getB1());
+		target.setAA(sourceHigh.getB2());
+		target.setAB(sourceHigh.getB3());
+		target.setAC(sourceHigh.getB4());
+		target.setAD(sourceHigh.getB5());
+		target.setAE(sourceHigh.getB6());
+		target.setAF(sourceHigh.getB7());
 	}
 
 	public static void DataOutToAIn(final IEightBitDataOut sourceLow,
